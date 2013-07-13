7e5239be18d9bad594fccdcec351718087456774
Jen Gilbert <jgilbert@arubanetworks.com>
2013-07-13 00:23:14 -0700

    Set up basic app structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GoodToGo::Application.config.secret_key_base = '153f81a3cbefed0c17414c0d33f67f8d26ff065b16ebf4025aab09ca6d99ea38815aaa213e6efea3cd9d0f62bf1c2ba430de2448791dd73de7a62b3db2fbef8c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GoodToGo::Application.config.session_store :cookie_store, key: '_good_to_go_session'
